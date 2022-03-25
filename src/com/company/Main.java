90197cd194488452e2a7e2760db28f80ce18587f
Pavel <pvk19941@rambler.ru>
2022-03-25 20:51:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Person pasha = new Person();
+        pasha.height = 120;
+        pasha.say("Harry");
+        System.out.println(pasha.height);
+        Person vlad = new Person();
+        System.out.println(vlad.height);
+
+
+
+    }
+}
